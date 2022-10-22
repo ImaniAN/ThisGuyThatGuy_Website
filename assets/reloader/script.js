591f7188587f8b8b9e985a2acd287bef1a6f0ef3
ImaniAN <32896144+ImaniAN@users.noreply.github.com>
2022-10-22 12:58:05 +0200

    Smaller updated

--- assets/reloader/script.js
+++
@@ -1,2 +0,0 @@
-
-var initiated=!1;$(document).on("add.cards changeParameter.cards",function(a){$(a.target).is(".twitter")&&!initiated&&(initiated=!0,setTimeout(function(){window.twttr.widgets.load();initiated=!1},2E3))});
--- assets/soundcloud-plugin/script.js
+++
@@ -1,2 +0,0 @@
-
-(function(a){if(a("html").hasClass("is-builder"))a(document).on("changeParameter.cards",function(b){a(b.target).find("iframe.mbr-embedded-video")&&a(window).resize()})})(jQuery);
